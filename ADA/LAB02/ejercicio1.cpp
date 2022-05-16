554f7af032c0e67b2bac13ee2e1a8e930bf02fb5
Yamil <yamil250100@gmail.com>
2022-05-16 14:20:45 -0500

    Update files

--- ADA/LAB02/ejercicio1.cpp
+++ ADA/LAB02/ejercicio1.cpp
@@ -1,52 +1,72 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-void mezclar(int a[], int b[], int c[], int na, int nb){
-	int i=0,j=0,k=0;
-	while(i<na || j<nb){
-		if(i<na && j<nb){
-			if(a[i]<b[j]){
-				c[k++]=a[i++];
-			}
-			else{
-				c[k++]=b[j++];
-			}
-		}else if(i<na){
-			c[k++]=a[i++];
-		}else{
-			c[k++]=b[j++];
+template <class T>
+void print_vector(const std::vector<T>& v){
+	for(const auto &i : v){
+		std::cout << i << ' ';
+	}
+	std::cout << '\n';
+}
+
+template <class T>
+void merge(std::vector<T>& v, int left_index, int mid_index, int right_index){
+	int i, j, k;
+	int left_size = mid_index - left_index + 1;
+	int right_size = right_index - mid_index;
+
+	std::vector<T> left_vector(left_size), right_vector(right_size);
+	for(i = 0; i < left_size; i++){
+		left_vector[i] = v[left_index + i];
+	}
+
+	for(j = 0; j < right_size; j++){
+		right_vector[j] = v[mid_index + 1 + j];
+	}
+
+	i = 0, j = 0, k = left_index;
+	while((i<left_size) and (j<right_size)){
+		if(left_vector[i] <= right_vector[j]){
+			v[k] = left_vector[i];
+			i++;
 		}
+		else{
+			v[k] = right_vector[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i < left_size){
+		v[k] = left_vector[i];
+		i++;
+		k++;
+	}
+
+	while(i < right_size){
+		v[k] = right_vector[j];
+		j++;
+		k++;
 	}
 }
 
-void ordenMezcla(int s[], int dest[],int a, int b){
-	int p,i;
-	if(a>=b)
-		return;
-	p=(a+b)/2;  
-	for(i=a;i<=b;i++){
-		dest[i]=s[i];
-	}
-	ordenMezcla(dest,s,a,p);
-	ordenMezcla(dest,s,p+1,b);
-	mezclar(&s[a],&s[p+1],&dest[a],p-a+1,b-p);
-}
+template <class T>
+void merge_sort(std::vector<T>& v, int left_index, int right_index){
+	int mid_index;
+	if(left_index < right_index){
+		mid_index = (left_index + right_index) / 2;
+		merge_sort(v, left_index, mid_index);
+		merge_sort(v, mid_index + 1, right_index);
 
-void printArray(int A[], int size){ 
-	int i; 
-	for (i=0; i < size; i++) 
-		printf("%d ", A[i]); 
-	printf("\n"); 
-} 
+		merge(v, left_index, mid_index, right_index);
+	}
+}
 
 int main(){
-	int arr[] = {6,3,8,5,7,1,4,6,3}; 
-	int arr_size = sizeof(arr)/sizeof(arr[0]); 
-	int dest[arr_size];
-	printArray(arr, arr_size); 
-	
-	ordenMezcla(arr,dest, 0, arr_size - 1); 
+	std::vector<int> k = {7, 2, 14, 9, 24, 19, 38, 32, 56, 42};
+	print_vector(k);
+	merge_sort(k, 0, k.size()-1);
+	print_vector(k);
 
-	printArray(dest, arr_size); 
+
 	return 0;
 }
