dad010985248084003ac18ea5e1ebf9b1500ca6b
YLlampi <yamil250100@gmail.com>
2022-08-01 22:40:24 -0500

    lab12 created

---
+++ ADA/LAB12/LAB12_Ejercicio01.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m; cin>>n>>m;
+    
+    vector<vector<int>> graph(n+1);
+
+    for(int i = 0; i < m; i++){
+        int a, b; cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    for(int i = 1; i <= n; i++){
+        cout << i << "-> ";
+        for(auto j : graph[i]){
+            cout << j << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
