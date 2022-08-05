410e4a8550c5415744cec00fc7401d2707421fa3
YLlampi <yamil250100@gmail.com>
2022-08-04 22:37:39 -0500

    lab12

--- ADA/LAB12/LAB12_Ejercicio01.cpp
+++ ADA/LAB12/LAB12_Ejercicio01.cpp
@@ -1,24 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, m; cin>>n>>m;
-    
-    vector<vector<int>> graph(n+1);
+class Game{
+    private:
+        int n; // Tamaño de la matriz
+        vector <vector <char>> table; // tablero
 
-    for(int i = 0; i < m; i++){
-        int a, b; cin>>a>>b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
+        vector <bool> visited;
+        vector <vector <int>> graph;
 
-    for(int i = 1; i <= n; i++){
-        cout << i << "-> ";
-        for(auto j : graph[i]){
-            cout << j << ' ';
+    public:
+        Game(int n) : n{n}{
+            vector <vector <char>> tableTemp(n, vector<char>(n, 45)); // 45 ASCII => -
+            this->table.assign(tableTemp.begin(), tableTemp.end());
+            generarTablero();
         }
-        cout << '\n';
-    }
+
+        void mainController(){
+            int a,b;
+            bool band = true;
+            do{
+                cout << "Posicion [X][Y]: "; cin>>a>>b;
+                a--;
+                b--;
+                if(this->table[a][b] != 42){
+                    
+                }
+                else {
+                    cout << "Game Over" << '\n';
+                    band = false;
+                }
+            }while(band);
+
+        }
+
+        void generarTablero() {
+            for(int i = 0; i < n; i++){
+                int numAleatorioI = 0 + rand() % (n);
+                int numAleatorioJ = 0 + rand() % (n);
+                this->table[numAleatorioI][numAleatorioJ] = 42; // 42 ASCII => * | Minas
+            }
+        }
+
+        void bfs(int v){
+            queue <int> Q;
+            Q.push(v);
+
+            while (not Q.empty()){ //mientras nuestra lista tenga nodos
+                int u = Q.front(); //seleccionamos el primer nodo de la lista
+                Q.pop(); //y lo eliminamos
+
+                if (not visited[u]){ //si no lo hemos visitado
+                    visited[u] = true;
+
+                    for (int i = 0; i < graph[u].size(); ++i){
+                        int w = graph[u][i];
+
+                        Q.push(w); //ponemos a sus vecinos en la lista
+                    }
+                }
+            }
+        }
+
+        void generateScript(){
+            cout << '\n';
+            cout << '+';
+            for(int i = 0; i < n*2 -1; i++){
+                cout << '-';
+            }
+            cout << '+';
+            cout << '\n';
+        }
+
+        void printTable() {
+            generateScript();
+            for(int i = 0; i < n; i++){
+                cout << '|';
+                for(int j = 0; j < n; j++){
+                    cout << this->table[i][j] << '|';
+                }
+                generateScript();
+            }
+        }
+
+};
+
+int main(){
+    srand(time(nullptr));
+
+    int n;
+    cout << "Size of Table: "; cin>>n;
+
+    Game myGame(n);
+
+    myGame.printTable();
+
+    //myGame.mainController();
 
     return 0;
 }
--- ADA/LAB12/main.cpp
+++ ADA/LAB12/main.cpp
@@ -1,7 +1,75 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
 using namespace std;
 
-int main(){
-    cout << "Hola mundo" << '\n';
+const int N = 15;
+const int dx[] = {0, 0, 1, 1, 1, -1, -1, -1};//up down down
+const int dy[] = {1, -1, -1, 0, 1, -1, 0, 1};
+char map[N][N];
+int n;
+struct Node
+{
+    int x, y;
+    Node() {}
+    Node(int a, int b){
+        x = a;
+        y = b;
+    }
+};
+
+void bfs(int x, int y)
+{
+    queue<Node> q;
+    q.push(Node(x, y));
+    while(!q.empty()){
+        Node tmp = q.front(), tmp1;
+        if(map[tmp.x][tmp.y] == '0')
+        for(int i = 0 ; i < 8; i ++){
+            tmp1.x = tmp.x + dx[i]; tmp1.y = tmp.y + dy[i];
+            if(tmp1.x < 1 || tmp1.x > n || tmp1.y < 1 || tmp1.y > n || map[tmp1.x][tmp1.y] == '@') continue;
+            q.push(tmp1);
+        }
+        map[tmp.x][tmp.y] = '@';
+        q.pop();
+    }
+    return ;
+}
+int main()
+{
+    int T, k = 1;
+    cin >> T;
+    while(T --){
+        cin >> n;
+        getchar();
+        for(int i = 1; i <= n; i ++){
+            for(int j  = 1; j <= n; j ++){
+                scanf("%c", &map[i][j]);
+            }
+            getchar();
+        }
+        int ans = 0;
+        for(int i = 1; i <= n; i ++){
+            for(int j = 1; j <= n; j ++){
+                if(map[i][j] == '0'){
+                    bfs(i, j);
+                    ans ++;
+                }
+
+            }
+        }
+        for(int i = 1; i <= n; i ++){
+            for(int j = 1; j <= n; j ++){
+                if(map[i][j] != '@'){
+                    ans ++;
+                }
+            }
+        }
+        cout << "Case " << k ++ << ": ";
+        cout << ans << endl;
+    }
     return 0;
 }
